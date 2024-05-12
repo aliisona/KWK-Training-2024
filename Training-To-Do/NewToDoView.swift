afef314117f13670a56a1427942de122b7777b8b
alisona <alisonahle@gmail.com>
2024-05-12 12:19:46 -0400

    Complete Iteration 0 :)

---
+++ Training-To-Do/NewToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  NewToDoView.swift
+//  Training-To-Do
+//
+//  Created by Scholar on 5/12/24.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewToDoView()
+}
---
+++ Training-To-Do/ToDoItem.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoItem.swift
+//  Training-To-Do
+//
+//  Created by Scholar on 5/12/24.
+//
+
+import Foundation
